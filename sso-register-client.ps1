306e35debf01dddfa93e046525ac31b9488c0ed6
jeps <petteri.stenius@ubisecure.com>
2018-09-24 16:59:24 +0300

    using SSO at login.example.ubidemo.com

--- sso-register-client.ps1
+++ sso-register-client.ps1
@@ -1,34 +1,45 @@
 [CmdletBinding()]
 param(
-    [parameter()] [uri] $Uri = "https://sso.example.com:8443",
-    [parameter()] [uri] $ManageUri = $Uri,
+    [parameter()] [uri] $Uri = "https://login.example.ubidemo.com",
+    [parameter()] [uri] $ManageUri = "https://manage.example.ubidemo.com",
     [parameter()] [uri] $RedirectUri = "http://localhost:19282/signin-oidc"
 )
 
 Import-Module "oauth2"
 Import-Module "sso-api-v2"
 
-New-OAuthClientConfig -Name "bootstrap.json" | New-SSOLogon -Uri $Uri -ManageUri $ManageUri -Code:$false -Username "system"
+$registration = @{
+"ClientId"="public"
+"ClientSecret"=(ConvertTo-SecureString -String "public" -AsPlainText -Force)
+"RedirectUri"="http://localhost/public"
+}
 
-Get-OAuthMetadata -Authority "$Uri/uas" | ConvertTo-Json | Set-Content -Path "openid-configuration.json" -Force
+New-OAuthClientConfig @registration | New-SSOLogon -Uri $Uri -ManageUri $ManageUri -Browser "default" 
+
+Get-OAuthMetadata -Authority ([uri]::new($Uri, "/uas")) | ConvertTo-Json | Set-Content -Path "openid-configuration.json" -Force
 
 $password1 = Get-SSOObject -Type "method" "password.1" -ErrorAction Stop
+$sms1 = Get-SSOObject -Type "method" "sms.1" -ErrorAction Stop
+$smtp1 = Get-SSOObject -Type "method" "smtp.1" -ErrorAction Stop
 $allusers = Get-SSOObject -Type "group" "System","Authenticated Users" -ErrorAction Stop
 
 $site = Set-SSOObject -Type "site" "AspNetCoreSample"
-$site | Set-SSOLink -Link $password1 | Out-Null
+$password1,$sms1,$smtp1 | Set-SSOLink -Link $site | Out-Null
 
 $users = $site | Set-SSOChild -ChildType "group" "users"
 $users | Set-SSOLink -LinkName "member" -Link $allusers | Out-Null
+$sms1,$smtp1 | Set-SSOLink -Link $users | Out-Null
 
 $policy = $site | Set-SSOChild -ChildType "policy" "policy"
 
 $app = $site | Set-SSOChild -ChildType "application" "application" -Enabled
 $app | Set-SSOLink -LinkName "allowedTo" -Link $users | Out-Null
 $app | Set-SSOLink -Link $policy | Out-Null
-$app | Set-SSOLink -Link $password1 -Enabled | Out-Null
+$password1,$sms1,$smtp1 | Set-SSOLink -Link $app -Enabled | Out-Null
 
-$request = @{"redirect_uris"=@($RedirectUri)} | ConvertTo-Json
+$request = @{
+"redirect_uris"=@($RedirectUri)
+} | ConvertTo-Json
 $response = $app | Set-SSOAttribute -Name "metadata" -ContentType "application/json" -Body $request
 
 $response | ConvertTo-Json | Set-Content -Path "client-config.json" -Force
